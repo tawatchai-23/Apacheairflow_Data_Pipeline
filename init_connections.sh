dff94061d2ee42418f3dcdebed0d69d62f8ce192
Tawatchai Pitak <jokjokjok00720@gmail.com>
2025-06-09 09:24:14 +0700

    Update init_connections.sh

--- init_connections.sh
+++ init_connections.sh
@@ -5,10 +5,10 @@ echo "🔗 Initializing Airflow Connections..."
 airflow connections delete 'ex_postgres' || true
 
 airflow connections add 'ex_postgres' \
-    --conn-type 'postgres' \
-    --conn-host '172.27.154.25' \
-    --conn-login 'gi.joke' \
-    --conn-password 'Tawatcha1@2021' \
-    --conn-schema 'opendata' \
-    --conn-port 5432 \
+    --conn-type 'postgres_type' \
+    --conn-host 'xxx.xx.xxx.xx' \
+    --conn-login 'xxxx' \
+    --conn-password 'xxxxxxxx' \
+    --conn-schema 'xxxxxxx' \
+    --conn-port xxxx \
     --conn-extra '{"options": "-c search_path=airflow"}'
